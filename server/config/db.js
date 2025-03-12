fe4ac3d0def13197b9572b7c184856fdc61dcc5b
RAVI GANGWAR <kingravin001@gmail.com>
2025-03-12 16:32:13 +0530

    updated again connectdb

--- server/config/db.js
+++ server/config/db.js
@@ -6,8 +6,6 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
             serverSelectionTimeoutMS: 30000, // 30 seconds
             socketTimeoutMS: 45000, // 45 seconds
         });
