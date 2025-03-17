bd1f2857b1160f7728e1f789633f7b3dda8996f0
RAVI GANGWAR <kingravin001@gmail.com>
2025-03-17 20:41:32 +0530

    Server updated for Default Room

--- server/controllers/defaultRoomController.js
+++ server/controllers/defaultRoomController.js
@@ -3,8 +3,13 @@ overrideConsole();
 
 const DefaultRoom = require('../models/DefaultRoom');
 
+const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+
 exports.getDefaultRoom = async (req, res) => {
     try {
+        // Add a delay of 5 seconds (10000 milliseconds)
+        await delay(10000);
+
         const defaultRoom = await DefaultRoom.findOne();
         if (!defaultRoom) {
             return res.status(404).json({ error: 'Default room not found' });
